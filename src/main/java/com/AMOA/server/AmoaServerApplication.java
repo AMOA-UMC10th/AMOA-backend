970a80ca39149677255db964197293d93f00a44b
Kim-DongH <kate3988@gmail.com>
2026-06-30 15:58:37 +0900

    feat: 프로젝트 초기 개발 환경 구축

---
+++ src/main/java/com/AMOA/server/AmoaServerApplication.java
@@ -0,0 +1,13 @@
+package com.AMOA.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmoaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmoaServerApplication.class, args);
+	}
+
+}
