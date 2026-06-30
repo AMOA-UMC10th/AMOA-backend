4e5085974e4a130a609913dc361c71a253ecc42f
Kim-DongH <kate3988@gmail.com>
2026-06-30 16:39:59 +0900

    fix: JWT 구현 전 Swagger 인증 설정 제거

--- src/main/java/com/AMOA/server/global/config/SwaggerConfig.java
+++ src/main/java/com/AMOA/server/global/config/SwaggerConfig.java
@@ -1,10 +1,7 @@
 package com.AMOA.server.global.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,21 +13,22 @@ public class SwaggerConfig {
     public OpenAPI swagger() {
         Info info = new Info().title("AMOA").description("AMOA API 명세서").version("0.0.1");
 
-        // JWT 토큰 헤더 방식
-        String securityScheme = "JWT TOKEN";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
-
-        Components components = new Components()
-                .addSecuritySchemes(securityScheme, new SecurityScheme()
-                        .name(securityScheme)
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("Bearer")
-                        .bearerFormat("JWT"));
+        //JWT 구현시 주석 제거
+//        // JWT 토큰 헤더 방식
+//        String securityScheme = "JWT TOKEN";
+//        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+//
+//        Components components = new Components()
+//                .addSecuritySchemes(securityScheme, new SecurityScheme()
+//                        .name(securityScheme)
+//                        .type(SecurityScheme.Type.HTTP)
+//                        .scheme("Bearer")
+//                        .bearerFormat("JWT"));
 
         return new OpenAPI()
                 .info(info)
                 .addServersItem(new Server().url("/"))
-                .addSecurityItem(securityRequirement)
-                .components(components);
+//                .addSecurityItem(securityRequirement)
+//                .components(components);
     }
 }
