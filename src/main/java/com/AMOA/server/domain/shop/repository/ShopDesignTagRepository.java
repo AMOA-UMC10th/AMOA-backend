28e4c91e465248aa0121e793565eb8819e157720
daeunkim701 <lionboot2025@gmail.com>
2026-07-12 18:29:02 +0900

    feat: Shop, ShopDesignTag 레포지토리 구현

--- src/main/java/com/AMOA/server/domain/shop/repository/ShopDesignTagRepository.java
+++ src/main/java/com/AMOA/server/domain/shop/repository/ShopDesignTagRepository.java
@@ -1,4 +1,17 @@
 package com.AMOA.server.domain.shop.repository;
 
-public interface ShopDesignTagRepository {
+import com.AMOA.server.domain.shop.entity.ShopDesignTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ShopDesignTagRepository extends JpaRepository<ShopDesignTag, Long> {
+
+    // 샵 ID로 연결된 태그 전체 조회
+    List<ShopDesignTag> findByShop_ShopId(Long shopId);
+
+    // 샵 ID로 연결된 태그 전체 삭제 (수정 시 사용)
+    void deleteByShop_ShopId(Long shopId);
 }
