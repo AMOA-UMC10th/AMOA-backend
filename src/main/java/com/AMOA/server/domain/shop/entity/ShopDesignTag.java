a78452ba613407f653eac03ff2389eb2c0f0f93f
daeunkim701 <lionboot2025@gmail.com>
2026-07-12 18:28:13 +0900

    feat: Shop, ShopDesignTag 엔티티 구현

--- src/main/java/com/AMOA/server/domain/shop/entity/ShopDesignTag.java
+++ src/main/java/com/AMOA/server/domain/shop/entity/ShopDesignTag.java
@@ -1,4 +1,27 @@
 package com.AMOA.server.domain.shop.entity;
 
+import com.AMOA.server.domain.common.entity.DesignTag;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "shop_design_tag")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class ShopDesignTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shop_designtag_id")
+    private Long shopDesignTagId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id", nullable = false)
+    private Shop shop;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "designtag_id", nullable = false)
+    private DesignTag designTag;
 }
