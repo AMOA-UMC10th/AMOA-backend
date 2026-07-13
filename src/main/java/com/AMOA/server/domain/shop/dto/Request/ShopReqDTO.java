b49839cb05296c2561baf29116a682e9bafa8233
daeunkim701 <lionboot2025@gmail.com>
2026-07-13 15:06:53 +0900

    feat: ShopReqDTO 구현

--- src/main/java/com/AMOA/server/domain/shop/dto/Request/ShopReqDTO.java
+++ src/main/java/com/AMOA/server/domain/shop/dto/Request/ShopReqDTO.java
@@ -1,4 +1,18 @@
 package com.AMOA.server.domain.shop.dto.Request;
 
+import java.util.List;
+
 public class ShopReqDTO {
+
+    // POST /api/admin/shops (샵 등록)
+    public record CreateShopRequest(
+            String shopName,
+            String kakaoChannelUrl,
+            String instagramUrl,
+            String profileImageUrl,
+            String address,
+            String shopPhoneNumber,
+            String businessHours,
+            List<Long> designtagIds
+    ) {}
 }
