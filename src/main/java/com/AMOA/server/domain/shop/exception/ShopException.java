55278d7d020189d1793e598b51b98466c09e9afe
daeunkim701 <lionboot2025@gmail.com>
2026-07-13 22:58:53 +0900

    fix: ShopException 단일 인수 생성자 누락 수정

--- src/main/java/com/AMOA/server/domain/shop/exception/ShopException.java
+++ src/main/java/com/AMOA/server/domain/shop/exception/ShopException.java
@@ -8,6 +8,13 @@ public class ShopException extends RuntimeException {
 
   private final BaseErrorCode errorCode;
 
+  // 인수 1개
+  public ShopException(BaseErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+
+  // 인수 2개
   public ShopException(BaseErrorCode errorCode, Throwable cause) {
     super(errorCode.getMessage(), cause);
     this.errorCode = errorCode;
