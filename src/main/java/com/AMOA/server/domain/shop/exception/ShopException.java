24676259e73e61f52523fd7bb0d07357d3e6c1da
daeunkim701 <lionboot2025@gmail.com>
2026-07-13 15:05:35 +0900

    feat: shop - errorcode 및 exception 작성

--- src/main/java/com/AMOA/server/domain/shop/exception/ShopException.java
+++ src/main/java/com/AMOA/server/domain/shop/exception/ShopException.java
@@ -1,7 +1,15 @@
 package com.AMOA.server.domain.shop.exception;
 
+import com.AMOA.server.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
 public class ShopException extends RuntimeException {
-  public ShopException(String message) {
-    super(message);
+
+  private final BaseErrorCode errorCode;
+
+  public ShopException(BaseErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
   }
 }
--- src/main/java/com/AMOA/server/domain/shop/exception/code/ShopErrorCode.java
+++ src/main/java/com/AMOA/server/domain/shop/exception/code/ShopErrorCode.java
@@ -1,4 +1,22 @@
 package com.AMOA.server.domain.shop.exception.code;
 
-public enum ShopErrorCode {
+import com.AMOA.server.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ShopErrorCode implements BaseErrorCode {
+
+    SHOP_NOT_FOUND(HttpStatus.NOT_FOUND, "SHOP404", "존재하지 않는 샵입니다."),
+    SHOP_INVALID_STATUS(HttpStatus.BAD_REQUEST, "SHOP400", "유효하지 않은 상태값입니다."),
+    SHOP_INVALID_ADDRESS(HttpStatus.BAD_REQUEST, "SHOP400", "주소를 좌표로 변환할 수 없습니다."),
+    SHOP_INVALID_DESIGNTAG(HttpStatus.BAD_REQUEST, "SHOP400", "존재하지 않는 디자인태그 ID입니다."),
+    SHOP_KEYWORD_EMPTY(HttpStatus.BAD_REQUEST, "SHOP400", "검색어를 입력해주세요."),
+    KAKAO_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SHOP500", "카카오 로컬 API 호출 중 오류가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
 }
