f7cab8f10b6972d3cdc9d34ae28d3bcd76d84118
daeunkim701 <lionboot2025@gmail.com>
2026-07-13 18:06:15 +0900

    fix: 코드리뷰 - Req에 검증코드 추가

--- src/main/java/com/AMOA/server/domain/shop/dto/Request/ShopReqDTO.java
+++ src/main/java/com/AMOA/server/domain/shop/dto/Request/ShopReqDTO.java
@@ -1,18 +1,26 @@
 package com.AMOA.server.domain.shop.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 
 public class ShopReqDTO {
 
-    // POST /api/admin/shops (샵 등록)
     public record CreateShopRequest(
+            @NotBlank(message = "샵 이름은 필수입니다.")
             String shopName,
+
+            @NotBlank(message = "카카오톡 채널 URL은 필수입니다.")
             String kakaoChannelUrl,
+
             String instagramUrl,
             String profileImageUrl,
+
+            @NotBlank(message = "주소는 필수입니다.")
             String address,
+
             String shopPhoneNumber,
             String businessHours,
+
             List<Long> designtagIds
     ) {}
 }
