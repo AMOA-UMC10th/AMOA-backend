9d69a735082d2bb35549dbffd36c4ffd0789a360
Kim-DongH <kate3988@gmail.com>
2026-07-14 13:53:35 +0900

    fix:error, success코드 반환 순서 수정

--- src/main/java/com/amoa/server/domain/user/exception/code/UserSuccessCode.java
+++ src/main/java/com/amoa/server/domain/user/exception/code/UserSuccessCode.java
@@ -9,16 +9,16 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum UserSuccessCode implements BaseSuccessCode {
     AUTH_LOGIN_OK(HttpStatus.OK,
-            "성공적으로 로그인에 성공하였습니다.",
-            "MEMBER200_1"
+            "MEMBER200_1",
+            "성공적으로 로그인에 성공하였습니다."
     ),
     SAVED_PLACE_CREATE_OK(
             HttpStatus.OK,
-            "장소가 성공적으로 저장되었습니다.",
-            "MEMBER201_1"
+            "MEMBER201_1",
+            "장소가 성공적으로 저장되었습니다."
     );
 
     private final HttpStatus httpStatus;
-    private final String message;
     private final String code;
+    private final String message;
 }
