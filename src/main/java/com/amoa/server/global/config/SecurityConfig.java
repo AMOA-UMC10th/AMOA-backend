3a5ebf1fa1d66a4d63dac3909774dca7f3a0aa8c
Kim-DongH <kate3988@gmail.com>
2026-07-16 21:55:16 +0900

    "fix: 코드래빗리뷰 수정"

--- src/main/java/com/amoa/server/global/config/SecurityConfig.java
+++ src/main/java/com/amoa/server/global/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
             //공통
             "/error",
             "/api/v1/auth/kakao",
-            "api/v1/auth/reissue",
+            "/api/v1/auth/reissue",
 
             // 인증 관련해서는 jwt 토큰 인증 없이도 요청을 보낼 수 있어야 함
             "/health"
