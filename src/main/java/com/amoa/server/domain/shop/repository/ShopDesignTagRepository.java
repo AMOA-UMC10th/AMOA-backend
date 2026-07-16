c0cabbd791eae78a3fc0f5e37d529105aa08cb24
daeunkim701 <lionboot2025@gmail.com>
2026-07-16 16:52:17 +0900

    perf: N+1 쿼리 방지를 위한 ShopDesignTag EntityGraph 추가

--- src/main/java/com/amoa/server/domain/shop/repository/ShopDesignTagRepository.java
+++ src/main/java/com/amoa/server/domain/shop/repository/ShopDesignTagRepository.java
@@ -1,6 +1,7 @@
 package com.amoa.server.domain.shop.repository;
 
 import com.amoa.server.domain.shop.entity.mapping.ShopDesignTag;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public interface ShopDesignTagRepository extends JpaRepository<ShopDesignTag, Long> {
 
     // 샵 ID로 연결된 태그 전체 조회
+    @EntityGraph(attributePaths = {"designTag"})
     List<ShopDesignTag> findByShop_ShopId(Long shopId);
 
     // 샵 ID로 연결된 태그 전체 삭제 (수정 시 사용)
