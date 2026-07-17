9857e7cfd944e7265bed1c3ee55d70dd6dbdc1da
Kevin7111 <146373958+Kevin7111@users.noreply.github.com>
2026-07-17 17:53:59 +0900

    refactor/#14-샵 찜 등록/취소 충돌 해결 (#29)
    * refactor/#14-샵 찜 등록/취소 충돌 해결
    
    * refactor: 코드 리뷰 반영 및 샵 찜 로직 개선
    
    * fix: 리뷰 반영
    
    * refactor: 컨트롤러 docs 추가 및 적용
    
    * fix: service 리뷰반영
    
    * fix: 리뷰 반영 및 예외처리 코드 수정 완료
    
    * fix: 리뷰반영

---
+++ src/main/java/com/amoa/server/domain/shop/entity/SavedShop.java
@@ -0,0 +1,51 @@
+package com.amoa.server.domain.shop.entity;
+
+import com.amoa.server.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Entity
+@Table(
+        name = "saved_shop",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_saved_shop_user_shop",
+                        columnNames = {"user_id", "shop_id"}
+                )
+        }
+        )
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SavedShop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_shop_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id", nullable = false)
+    private Shop shop;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    public SavedShop(User user, Shop shop){
+        this.user = user;
+        this.shop = shop;
+    }
+
+
+}
---
+++ src/main/java/com/amoa/server/domain/shop/service/command/SavedShopCommandService.java
@@ -0,0 +1,61 @@
+package com.amoa.server.domain.shop.service.command;
+
+import com.amoa.server.domain.shop.dto.Response.SavedShopResDTO;
+import com.amoa.server.domain.shop.entity.SavedShop;
+import com.amoa.server.domain.shop.entity.Shop;
+import com.amoa.server.domain.shop.exception.code.ShopErrorCode;
+import com.amoa.server.domain.shop.repository.SavedShopRepository;
+import com.amoa.server.domain.shop.repository.ShopRepository;
+import com.amoa.server.domain.user.entity.User;
+import com.amoa.server.global.apiPayload.exception.GeneralException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SavedShopCommandService {
+
+    private final SavedShopRepository savedShopRepository;
+    private final ShopRepository shopRepository;
+
+    @Transactional
+    public SavedShopResDTO.LikeResultDTO createShopLike(User user, Long shopId){
+
+        Shop shop = shopRepository.findById(shopId)
+                .orElseThrow(() -> new GeneralException(ShopErrorCode.SHOP_NOT_FOUND));
+
+        SavedShop savedShop = new SavedShop(user, shop);
+
+        try{
+            savedShopRepository.save(savedShop);
+        } catch (DataIntegrityViolationException e) {
+            throw new GeneralException((ShopErrorCode.SHOP_ALREADY_LIKED));
+        }
+
+        return SavedShopResDTO.LikeResultDTO.builder()
+                .userShopId(savedShop.getId())
+                .shopId(shop.getId())
+                .createdAt(savedShop.getCreatedAt())
+                .build();
+
+
+    }
+
+    @Transactional
+    public void deleteShopLike(User user, Long shopId){
+
+        Shop shop = shopRepository.findById(shopId)
+                .orElseThrow(() -> new GeneralException(ShopErrorCode.SHOP_NOT_FOUND));
+
+        SavedShop savedShop = savedShopRepository.findByUserAndShop(user, shop)
+                .orElseThrow(() -> new GeneralException(ShopErrorCode.SHOP_LIKE_NOT_FOUND));
+
+        savedShopRepository.delete(savedShop);
+
+        //Check ERD first
+        //shop.decreaseLikeCount();
+    }
+}
