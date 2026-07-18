3a64e1cb63ea7b6d4c7ce7231c5adcd061cc9eeb
daeunkim701 <lionboot2025@gmail.com>
2026-07-18 17:06:36 +0900

    feat: USER_NOT_FOUND 에러코드 추가

--- src/main/java/com/amoa/server/domain/shop/exception/code/ShopErrorCode.java
+++ src/main/java/com/amoa/server/domain/shop/exception/code/ShopErrorCode.java
@@ -16,6 +16,7 @@ public enum ShopErrorCode implements BaseErrorCode {
     SHOP_KEYWORD_EMPTY(HttpStatus.BAD_REQUEST, "SHOP400_4", "검색어를 입력해주세요."),
     SHOP_ALREADY_LIKED(HttpStatus.CONFLICT, "SHOP409_1", "이미 찜한 네일샵입니다."),
     SHOP_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "SHOP404_2", "찜한 네일샵을 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "SHOP404_4", "존재하지 않는 유저입니다."),
     KAKAO_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SHOP500_1", "카카오 로컬 API 호출 중 오류가 발생했습니다."),
     KAKAO_SEARCH_NOT_FOUND(HttpStatus.NOT_FOUND, "SHOP404_3", "검색 결과가 없습니다."),
     REGION_NOT_FOUND(HttpStatus.BAD_REQUEST, "SHOP400_5", "존재하지 않는 지역입니다.");
