52a731665468924a7a31e41b2d78a9839ee54c38
daeunkim701 <lionboot2025@gmail.com>
2026-07-18 22:50:02 +0900

    feat: 아트 목록 필터 시 사용되는 정렬 Enum

--- src/main/java/com/amoa/server/domain/common/enums/SortType.java
+++ src/main/java/com/amoa/server/domain/common/enums/SortType.java
@@ -1,4 +1,9 @@
 package com.amoa.server.domain.common.enums;
 
 public enum SortType {
+    RECOMMENDED,  // 추천순
+    POPULAR,      // 인기순
+    PRICE_ASC,    // 가격 낮은순
+    PRICE_DESC,   // 가격 높은순
+    LATEST        // 최신순
 }
