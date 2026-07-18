ce46ca7ef9f6f3fa53d4c1a4b3dd400dc3423d8b
daeunkim701 <lionboot2025@gmail.com>
2026-07-18 22:48:52 +0900

    feat: 샵 상세 카드 목록 조회 Service 구현

--- src/main/java/com/amoa/server/domain/shop/exception/code/ShopSuccessCode.java
+++ src/main/java/com/amoa/server/domain/shop/exception/code/ShopSuccessCode.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ShopSuccessCode implements BaseSuccessCode {
 
-    SHOP_CREATED(HttpStatus.CREATED, "SHOP201_1", "샵이 등록되었습니다."),
     SHOP_FOUND(HttpStatus.OK, "SHOP200_1", "샵 상세 조회에 성공했습니다."),
     SHOP_LIST_FOUND(HttpStatus.OK, "SHOP200_2", "샵 목록 조회에 성공했습니다."),
     SHOP_UPDATED(HttpStatus.OK, "SHOP200_3", "샵이 수정되었습니다."),
@@ -17,7 +16,9 @@ public enum ShopSuccessCode implements BaseSuccessCode {
     SHOP_LIKED(HttpStatus.OK, "SHOP200_5", "네일샵 찜 등록에 성공했습니다."),
     SHOP_UNLIKED(HttpStatus.OK, "SHOP200_6", "네일샵 찜 취소에 성공했습니다."),
     DESIGNTAG_LIST_FOUND(HttpStatus.OK, "SHOP200_7", "디자인태그 목록 조회에 성공했습니다."),
-    KAKAO_SEARCH_FOUND(HttpStatus.OK, "SHOP200_8", "카카오 로컬 API 검색에 성공했습니다.");
+    KAKAO_SEARCH_FOUND(HttpStatus.OK, "SHOP200_8", "카카오 로컬 API 검색에 성공했습니다."),
+    CARD_LIST_FOUND(HttpStatus.OK, "SHOP200_9", "카드 목록 조회에 성공했습니다."),
+    SHOP_CREATED(HttpStatus.CREATED, "SHOP201_1", "샵이 등록되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
