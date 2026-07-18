3dcff2551c679541598474afe3cdb47e30efafd8
cdth12345 <cdth7509@naver.com>
2026-07-18 16:03:09 +0900

    refactor: 디자인 태그, 아트 타입 리팩토링 (#35)

--- src/main/java/com/amoa/server/domain/card/enums/ArtDesign.java
+++
@@ -1,5 +0,0 @@
-package com.amoa.server.domain.card.enums;
-
-public enum ArtDesign {
-    SIMPLE, CHARMING, FANCY, STREET, UNIQUE, NATURAL, VINTAGE
-}
--- src/main/java/com/amoa/server/domain/common/enums/ArtType.java
+++ src/main/java/com/amoa/server/domain/common/enums/ArtType.java
@@ -1,4 +1,4 @@
-package com.amoa.server.domain.card.enums;
+package com.amoa.server.domain.common.enums;
 
 public enum ArtType {
     MONTHLY, ONE_COLOR, EVENT
