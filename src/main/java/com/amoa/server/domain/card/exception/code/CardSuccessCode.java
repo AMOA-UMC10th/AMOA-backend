5093e37f747644cd951b3731dd2166ae891668e0
Kevin7111 <jeonkevin71@gmail.com>
2026-07-19 15:43:34 +0900

    feat/#32- 카드 찜 등록/취소 시 좋아요 수 동기화

--- src/main/java/com/amoa/server/domain/card/exception/code/CardSuccessCode.java
+++ src/main/java/com/amoa/server/domain/card/exception/code/CardSuccessCode.java
@@ -11,7 +11,6 @@ public enum CardSuccessCode implements BaseSuccessCode {
     CARD_CREATED(HttpStatus.CREATED,
             "CARD201_1",
             "카드가 등록되었습니다."),
-
     CARD_LIKED(
             HttpStatus.OK,
             "CARD200_1",
@@ -21,9 +20,10 @@ public enum CardSuccessCode implements BaseSuccessCode {
             HttpStatus.OK,
             "CARD200_2",
             "카드 찜 취소에 성공했습니다."
+    ),
     CARD_KAKAO_CHANNEL_FOUND(
             HttpStatus.OK,
-            "CARD200_1",
+            "CARD200_3",
             "카카오톡 채널 URL을 성공적으로 조회했습니다."
     );
 
