3f7add6a1886526abdce55cb09a385888615999e
daeunkim701 <lionboot2025@gmail.com>
2026-07-22 01:26:58 +0900

    feat: converter 작성

--- src/main/java/com/amoa/server/domain/user/converter/UserConverter.java
+++ src/main/java/com/amoa/server/domain/user/converter/UserConverter.java
@@ -1,4 +1,10 @@
 package com.amoa.server.domain.user.converter;
 
+import com.amoa.server.domain.user.dto.response.NicknameCheckResDTO;
+
 public class UserConverter {
+
+    public static NicknameCheckResDTO toNicknameCheckResDTO(String nickname, boolean available) {
+        return new NicknameCheckResDTO(nickname, available);
+    }
 }
