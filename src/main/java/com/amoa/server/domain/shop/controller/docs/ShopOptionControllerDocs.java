86a0e36c6de95e0f2661b249aa12f4ba1178396e
daeunkim701 <lionboot2025@gmail.com>
2026-07-24 21:32:32 +0900

    refactor: API 명세서 기반 url 수정

--- src/main/java/com/amoa/server/domain/shop/controller/docs/ShopOptionControllerDocs.java
+++ src/main/java/com/amoa/server/domain/shop/controller/docs/ShopOptionControllerDocs.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Tag(name = "Shop", description = "샵 관련 API")
+@Tag(name = "Shop(admin)", description = "샵 관련 API(admin)")
 public interface ShopOptionControllerDocs {
 
     @Operation(
