c7bab44a9ec69cac921d7f27dbd0cf453807908a
daeunkim701 <lionboot2025@gmail.com>
2026-07-26 15:27:58 +0900

    refactor: 매칭 여부 확인 메서드 추가

--- src/main/java/com/amoa/server/domain/card/repository/CardDesignTagRepository.java
+++ src/main/java/com/amoa/server/domain/card/repository/CardDesignTagRepository.java
@@ -1,9 +1,14 @@
 package com.amoa.server.domain.card.repository;
 
 import com.amoa.server.domain.card.entity.mapping.CardDesignTag;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardDesignTagRepository extends JpaRepository<CardDesignTag, Long> {
+
+    // 매칭 여부 확인 메서드 추가
+    boolean existsByCard_IdAndDesignTag_IdIn(Long cardId, List<Long> designTagIds);
+
 }
