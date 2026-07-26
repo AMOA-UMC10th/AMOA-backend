dcd7fe9a42b63185a17b3213198b7a38c2341668
daeunkim701 <lionboot2025@gmail.com>
2026-07-26 15:30:22 +0900

    refactor: toCardListResponse 수정

--- src/main/java/com/amoa/server/domain/shop/converter/ShopConverter.java
+++ src/main/java/com/amoa/server/domain/shop/converter/ShopConverter.java
@@ -103,15 +103,18 @@ public class ShopConverter {
     // Card 목록 → CardListResponse DTO 변환
     public static ShopResDTO.CardListResponse toCardListResponse(
             Shop shop,
-            Page<Card> cards,
-            List<ShopResDTO.CardResponse> cardResponses) {
+            Long totalCount,
+            List<ShopResDTO.CardResponse> cardResponses,
+            String nextCursor,
+            boolean hasNext
+    ) {
         return new ShopResDTO.CardListResponse(
                 shop.getId(),
                 shop.getShopName(),
-                cards.getTotalElements(),
-                cards.getNumber(),
-                cards.getSize(),
-                cardResponses
+                totalCount,
+                cardResponses,
+                nextCursor,
+                hasNext
         );
     }
 
