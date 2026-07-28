c1c7434cd089f088f182772a6659e17884de6d86
김동희 | Donghee Kim <kate3988@gmail.com>
2026-07-28 12:53:08 +0900

    "fix: 온보딩 스킵 옵션이 가능하도록 지원" (#145)

--- src/main/java/com/amoa/server/domain/user/service/command/OnboardingCommandService.java
+++ src/main/java/com/amoa/server/domain/user/service/command/OnboardingCommandService.java
@@ -52,14 +52,17 @@ public class OnboardingCommandService {
 
         validateOnboardingUser(user);
         validateNickname(request.nickname(), userId);
-        validateRegionLimit(request.regionIds());
         validateDuplicateIds(request);
 
         List<DesignTag> designTags =
-                findDesignTags(request.designTagIds());
+                request.designTagIds().isEmpty()
+                        ? List.of()
+                        : findDesignTags(request.designTagIds());
 
         List<Region> regions =
-                findRegions(request.regionIds());
+                request.regionIds().isEmpty()
+                        ? List.of()
+                        : findRegions(request.regionIds());
 
         List<Term> terms =
                 findAndValidateTerms(request.agreements());
@@ -138,16 +141,6 @@ public class OnboardingCommandService {
         if (hasDuplicate(termIds)) {
             throw new UserException(
                     UserErrorCode.DUPLICATED_TERM_AGREEMENT
-            );
-        }
-    }
-
-    private void validateRegionLimit(
-            List<Long> regionIds
-    ) {
-        if (regionIds.size() > 3) {
-            throw new UserException(
-                    UserErrorCode.INTERESTED_REGION_LIMIT_EXCEEDED
             );
         }
     }
