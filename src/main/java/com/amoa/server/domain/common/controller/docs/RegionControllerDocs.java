89eaa5c45774d3156cfcc330fc74fbd17b132048
cdth12345 <cdth7509@naver.com>
2026-08-11 05:20:37 +0900

    refactor: 스웨거 설명 수정

--- src/main/java/com/amoa/server/domain/common/controller/docs/RegionControllerDocs.java
+++ src/main/java/com/amoa/server/domain/common/controller/docs/RegionControllerDocs.java
@@ -13,7 +13,11 @@ public interface RegionControllerDocs {
 
     @Operation(
             summary = "검색 기반 지역 조회",
-            description = "키워드를 기반으로 등록된 법정동 지역 목록을 조회합니다."
+            description = """
+                    키워드를 기반으로 등록된 법정동 지역 목록을 조회합니다.
+                    
+                    - 키워드로 검색에 성공했지만, 결과가 없으면 빈 리스트를 반환합니다.
+                    """
     )
     ApiResponse<List<RegionInfo>> search(
             @RequestParam String keyword
